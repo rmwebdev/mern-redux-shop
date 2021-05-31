78b0f5915ef7f90e7623e9b92482b8beb04a8a6e
Hierro Nabit <roninabit@gmail.com>
2021-06-01 01:37:53 +0700

    setup navbar and footer frontend using react-bootrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      <h>Welcome BRO</h>
-    </div>
+    <>
+      <Header/>
+        <main className="py-3">
+          <Container>
+          <h4>Welcome broo</h4>
+          </Container>
+        </main>
+      <Footer />
+    </>
   )
 }
 
